057c03ef5421a8cc35428e21a7ed5e362195a8db
unknown <ijul@student.uns.ac.id>
2025-03-02 22:35:17 +0700

    init

---
+++ app/images.ts
@@ -0,0 +1,21 @@
+import iconUns from "/app/images/icon-uns.svg";
+import dpupr from "/app/images/dpuprWebGIS.png";
+import geopedia from "/app/images/geopedia.png";
+import ijulwebgis from "/app/images/ijulWebGIS.png";
+import moreProjects from "/app/images/moreProjects.png";
+import arcgis from "/app/images/arcgis.png";
+import qgis from "/app/images/qgis.png";
+import js from "/app/images/javascript.png";
+import gee from "/app/images/gee.png";
+
+export {
+  iconUns,
+  dpupr,
+  geopedia,
+  ijulwebgis,
+  moreProjects,
+  arcgis,
+  qgis,
+  js,
+  gee,
+};
